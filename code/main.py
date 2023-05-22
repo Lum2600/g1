2d876503fc6b6ab1c1d5fc6dcee0f530fb5e8c1a
Lum2600 <paolosimone06@gmail.com>
2023-05-22 18:51:31 +0200

    pausa + timer + riprova

--- code/main.py
+++ code/main.py
@@ -4,92 +4,171 @@ from pygame.locals import *
 from random import randint
 pygame.init()
 pygame.font.init()
+pygame.mixer.init()
+
+
+#schermo del gioco
 WINDOW_SIZE = (720, 1080)
 screen = pygame.display.set_mode(WINDOW_SIZE)
-game_over_screen = pygame.display.set_mode(WINDOW_SIZE)
-
-pygame.display.set_caption('Menu')
+pygame.display.set_caption('gioco')
 screen.fill((0, 200, 0))
 schermo = pygame.Surface((720, 1800))
 schermo.fill((0, 0, 200))
+sfondo = pygame.image.load('image\sfondoy.png')
+sfondo = pygame.transform.scale(sfondo, (720, 1080))
+#schermo del menu
+menu_screen = pygame.display.set_mode(WINDOW_SIZE)
+sfondo_menu = pygame.image.load('image/sfondoy.png')
+sfondo_menu = pygame.transform.scale(sfondo, (720, 1080))
+logo_image= pygame.image.load('image/lgo-removebg-preview.png')
+
+#schermo del gameover
+game_over_screen = pygame.display.set_mode(WINDOW_SIZE)
+sfondo_game_over = pygame.image.load("image\sfondo_over.jpg")
+sfondo_game_over = pygame.transform.scale(sfondo_game_over, (720, 1080))
+
+#musica
+ost = pygame.mixer.music.load("image\X2Download.app - 1 Hour of Relaxing Pokemon HeartGold_SoulSilver Music Compilation (128 kbps).mp3")
+pygame.mixer_music.set_volume(1.0)
+score_100 = pygame.mixer.Sound('image\Mario Jump Sound Effect.mp3')
+
+#costanti di gioco
 vel_vert = 10
 vel_orizz = 5
 font = pygame.font.SysFont('Arial', 40)
 score = 0
 clock = pygame.time.Clock()
-sfondo = pygame.image.load('g1/image/sfondoy.png')
-sfondo = pygame.transform.scale(sfondo, (720, 1080))
 fps = 160
-menu_screen = pygame.display.set_mode(WINDOW_SIZE)
-sfondo_menu = pygame.image.load('g1/image/sfondoy.png')
-sfondo_menu = pygame.transform.scale(sfondo, (720, 1080))
-vite_image = pygame.image.load('g1/image/Vite.png')
+vel_pokeball = 1.8
+vel_monete = 2.2
+num_collisioni = 0
+variabile_di_stato ="menu"
+TIMERSHOT = pygame.event.custom_type()
+pygame.time.set_timer(TIMERSHOT, 500)
+incremento_al_sec= 1
+
+#immagini png
+vite_image = pygame.image.load('image/Vite.png')
 vite_rect = pygame.Rect((5, 5), (80, 80) )
 vite_image = pygame.transform.scale(vite_image, (80, 80))
-moneta_image1 = pygame.image.load('g1/image/moneta-removebg-preview (1).png')
+moneta_image1 = pygame.image.load('image/moneta-removebg-preview (1).png')
 moneta_rect1 = pygame.Rect((randint(0, 450), 0), (80, 80) )
 moneta_image1 = pygame.transform.scale(moneta_image1,(80, 80))
-moneta_image2 = pygame.image.load('g1/image/moneta-removebg-preview (1).png')
+moneta_image2 = pygame.image.load('image/moneta-removebg-preview (1).png')
 moneta_rect2 = pygame.Rect((randint(500, 630), -100), (80, 80) )
 moneta_image2 = pygame.transform.scale(moneta_image1,(80, 80))
-pokeball_image1 = pygame.image.load('g1/image/pokeball.png')
-pokeball_image2 =pygame.image.load('g1/image/pokeball.png')
+pokeball_image1 = pygame.image.load('image/pokeball.png')
+pokeball_image2 =pygame.image.load('image/pokeball.png')
 pokeball_rect1 = pygame.Rect((randint(0, 450), -100), (60, 50) )
 pokeball_rect2 = pygame.Rect((randint(500, 660), -100), (60, 50) )
 pokeball_image1 = pygame.transform.scale(pokeball_image1,(60, 50))
 pokeball_image2 = pygame.transform.scale(pokeball_image2,(60, 50))
-vel_pokeball = 1.8
-vel_monete = 2.2
-sfondo_game_over = pygame.image.load("g1/image/sfondoy.png")
-sfondo_game_over = pygame.transform.flip(sfondo_game_over, False, True)
-sfondo_game_over = pygame.transform.scale(sfondo_game_over, (720, 1080))
+
+
+
+
 def myFunction():
     print('Button Pressed')
 
+#bottoni
+start = oggetti.Button((250, 225, 2),screen, 98, 390, 250, 80, 'Start', myFunction)
+skin = oggetti.Button((250, 225, 2),screen, 358, 390, 250, 80, 'skin', myFunction)
+dif_1 = oggetti.Button((250, 225, 2),screen, 205, 500, 100, 80, '1', myFunction)
+dif_2 = oggetti.Button((250, 225, 2),screen, 310, 500, 100, 80, '2', myFunction)
+dif_3 = oggetti.Button((250, 225, 2),screen, 415, 500, 100, 80, '3', myFunction)
+game_over = oggetti.Button((27, 42, 207),screen, 180, 550, 400, 100, 'Riprova', myFunction)
 
-start = oggetti.Button(screen, 98, 390, 250, 80, 'Start', myFunction)
-skin = oggetti.Button(screen, 358, 390, 250, 80, 'skin', myFunction)
-dif_1 = oggetti.Button(screen, 205, 500, 100, 80, '1', myFunction)
-dif_2 = oggetti.Button(screen, 310, 500, 100, 80, '2', myFunction)
-dif_3 = oggetti.Button(screen, 415, 500, 100, 80, '3', myFunction)
-game_over = oggetti.Button(screen, 180, 500, 400, 100, 'Hai perso', myFunction)
 
-logo_image= pygame.image.load('g1/image/lgo-removebg-preview.png')
-num_collisioni = 0
 
+#giocatore
 drago = oggetti.Drago(schermo, ((screen.get_width()//2), (schermo.get_height()//2)) , (140, 140), screen)
 
-variabile_di_stato ="menu"
+
 
 
 while True:
-    if variabile_di_stato =="gioco":
-        while True:
+    pygame.mixer.music.play(-1)
+    if variabile_di_stato=="menu":
+        
+        variabile_di_stato = "menu"
+        run = True
+        while run:
+            
+                
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-        
-    
             keys = pygame.key.get_pressed()
             if keys[K_BACKSPACE]:
+                    break
+
+            menu_screen.blit(sfondo_menu, (0,0))
+            screen.blit(logo_image, (180,100))
+            start.cliccare()
+            skin.cliccare()
+            dif_1.cliccare()
+            dif_2.cliccare()
+            dif_3.cliccare()
+            if start.action():
+                variabile_di_stato = "gioco"
+            if variabile_di_stato ==  "gioco":
+                run = False
                 break
-            elif keys[K_d]:
-                drago.move_right()
+            
+            print(variabile_di_stato)
+            pygame.display.update()
+            clock.tick(160)
+
+    
+    if variabile_di_stato =="gioco":
+        run = True
+        while run:
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == TIMERSHOT:
+                    score+=incremento_al_sec
+                    print("timer +")
+                
+                    
+                
+                
+                     
+            keys = pygame.key.get_pressed()
+
+            if keys[K_m]:
+                drago.pausa()
+                vel_monete = 0
+                vel_pokeball = 0
+                incremento_al_sec = 0
+                
+            if keys[K_n]: 
+                vel_pokeball = 1.8
+                vel_monete = 2.2
+                drago.btg()
+                incremento_al_sec = 1
+                
+            if keys[K_BACKSPACE]:
+                    break
+            if keys[K_d]:
+                    drago.move_right()
             else:
-                drago.stop_moving_right()
+                    drago.stop_moving_right()
             if keys[K_a]:
-                drago.move_left()
+                    drago.move_left()
             else:
-                drago.stop_moving_left() 
+                    drago.stop_moving_left() 
             if keys[K_w]:
-                drago.move_foward() 
+                    drago.move_foward() 
             else:
-                drago.stop_moving_foward()
+                    drago.stop_moving_foward()
             if keys[K_s]:
-                drago.move_back() 
+                    drago.move_back() 
             else:
-                drago.stop_moving_back()
+                    drago.stop_moving_back()
 
             screen.fill((0, 200, 0))
             screen.blit(schermo, ((screen.get_width()-schermo.get_width())//2, (screen.get_height()-schermo.get_height())//2 ))
@@ -97,7 +176,21 @@ while True:
             screen.blit(sfondo, (0, 0))
             drago.muovimento()
             drago.disegna()
+            
+            if (score+1000)%1000== 0 and score!=0:
+                pygame.mixer.Sound.set_volume(score_100, 0.1)
+                pygame.mixer.Sound.play(score_100)
+                
+                   
+            
+                
+                
+            # else:
+            #     pygame.mixer_music.load("image\X2Download.app - 1 Hour of Relaxing Pokemon HeartGold_SoulSilver Music Compilation (128 kbps).mp3")
+            #     pygame.mixer_music.set_volume(0.5)
+            #     pygame.mixer_music.play()
 
+                
 
             moneta_rect1.y+=vel_monete
             if moneta_rect1.y > screen.get_height(): 
@@ -153,8 +246,26 @@ while True:
                 screen.blit(vite_image, (5, 5))
             else:
                 variabile_di_stato = "game_over"
+                
                 if variabile_di_stato !="gioco":
+                    moneta_rect1.y = randint(-800, 0)
+                    moneta_rect1.x = randint(0, 450)
+                    score +=10
+                    moneta_rect2.y = randint(-800, 0)
+                    moneta_rect2.x = randint(500, 640)
+                    score +=10
+                    pokeball_rect1.y = randint(-800, 0)
+                    pokeball_rect1.x = randint(0, 450)
+                    num_collisioni+=1
+                    pokeball_rect2.y = randint(-800, 0)
+                    pokeball_rect2.x = randint(500, 640)
+                    num_collisioni+=1
+                    drago = oggetti.Drago(schermo, ((screen.get_width()//2), (schermo.get_height()//2)) , (140, 140), screen)
+                    num_collisioni = 0
+                    score = 0
+                    run = False
                     break
+                    
 
             screen.blit(moneta_image1, moneta_rect1)
             screen.blit(moneta_image2, moneta_rect2)
@@ -164,35 +275,31 @@ while True:
             screen.blit(pokeball_image2, pokeball_rect2)   
             pygame.display.update()
             clock.tick(160)
-    elif variabile_di_stato=="menu":
-        while True:
+    
+          
+    if variabile_di_stato=="game_over":
+        run = True  
+        while run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            menu_screen.blit(sfondo_menu, (0,0))
-            screen.blit(logo_image, (180,100))
-            start.cliccare()
-            skin.cliccare()
-            dif_1.cliccare()
-            dif_2.cliccare()
-            dif_3.cliccare()
-            variabile_di_stato = start.action()
-            if variabile_di_stato ==  "gioco":
-                break
-            pygame.display.update()
-            clock.tick(160)
-            
-    elif variabile_di_stato=="game_over":
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            game_over_screen.fill((0, 255, 0))
+
             game_over_screen.blit(sfondo_game_over, (0,0))
             screen.blit(logo_image, (180,80))
             game_over.cliccare()
+            if game_over.action():
+                variabile_di_stato = "menu"
+                print(variabile_di_stato)
+            
+            if variabile_di_stato == "menu":
+                run = False
+                break
+                            
+            print(variabile_di_stato)
+                
+            
+            
             pygame.display.update()
             clock.tick(160)
 
--- code/oggetti.py
+++ code/oggetti.py
@@ -6,7 +6,7 @@ pygame.font.init()
 class Drago():
     def __init__(self, schermo, pos, size, screen):
         self.dragoRect = pygame.Rect(pos[0], pos[1], size[0], size[1])
-        self.dragoimage = pygame.image.load('g1/image/drago-removebg-preview - Copia inv.png')
+        self.dragoimage = pygame.image.load('image/drago-removebg-preview - Copia inv.png')
         self.dragoimage = pygame.transform.scale(self.dragoimage, size)
         self.schermo = schermo
         self.screen  = screen
@@ -45,12 +45,12 @@ class Drago():
         
         if self.muovi_destra:   
             self.dragoRect.right += self.vel_orizz
-            
+            self.dragoimage = pygame.image.load('image/drago-removebg-preview - Copia inv.png')
             if self.dragoRect.right > self.screen.get_width():
                 self.dragoRect.right = self.screen.get_width()
                
         if self.muovi_sinistra:
-            self.dragoimage = pygame.transform.flip(self.dragoimage, True, False)
+            self.dragoimage = pygame.image.load('image\drago-removebg-preview.png')
             self.dragoRect.left -= self.vel_orizz
             if self.dragoRect.left < 0:
                 self.dragoRect.left = 0
@@ -70,6 +70,13 @@ class Drago():
         self.screen.blit(self.dragoimage, self.dragoRect)
     def returna_rect(self):
         return self.dragoRect
+    def pausa(self):
+        self.vel_orizz = 0
+        self.vel_vert = 0
+    def btg(self):
+        self.vel_vert = 1.7
+        self.vel_orizz = 1.7
+
     
 
 def collisione_moneta1(moneta1, drago):
@@ -92,19 +99,20 @@ def collisione_pokeball2(pokeball2, drago):
 
 font = pygame.font.SysFont('Arial', 40)
 class Button():
-    def __init__(self,screen, x, y, width, height, buttonText='Button', cliccami=None, uno=False):
+    def __init__(self,bg_color,screen, x, y, width, height, buttonText='Button', cliccami=None, uno=False):
         self.x = x
         self.y = y
+        self.bg_color=bg_color
         self.width = width
         self.height = height
-        self.cliccami = cliccami
-        self.uno = uno
+        self.cliccato = cliccami
+        self.stai_cliccando = uno
         self.schiacciato = False
         self.screen = screen
-        self.image = pygame.image.load('g1/image/lgo-removebg-preview.png')
+        self.image = pygame.image.load('image/lgo-removebg-preview.png')
         self.image = pygame.transform.scale(self.image, (350, 150))
         self.colora = {
-            'normale': '#ffffff',
+            'normale': self.bg_color,
             'sopra mouse': '#666666',
             'schiacciato': '#333333',
         }
@@ -123,20 +131,18 @@ class Button():
             self.buttonSurface.fill(self.colora['sopra mouse'])
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
                 self.buttonSurface.fill(self.colora['schiacciato'])
-                if self.uno:
-                    self.cliccami()
-                elif not self.schiacciato:
-                    self.cliccami()
-                    self.schiacciato = True
+                
+                self.schiacciato = True
+                self.cliccato()
+                
             else:
                 self.schiacciato = False
+
         self.buttonSurface.blit(self.buttonSurf, ((self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2), (self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2)))
         self.screen.blit(self.buttonSurface, self.buttonRect)
         
     def action(self):
         if self.schiacciato:
-            return "gioco"
-        else:
-            return "menu"
+            return True
     def logo(self):
         self.screen.blit(self.image, (180, 100))
